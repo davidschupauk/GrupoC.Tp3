81ec709e1a4134f45295a4162f19b4f770d9e538
jose <leorodrigul17@gmail.com>
2024-10-05 09:12:31 -0300

    Caso de Uso 2 Generar OS

--- CDU2/GenerarOrdenDeSeleccionForm.Designer.cs
+++ CDU2/GenerarOrdenDeSeleccionForm.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace GrupoC.Tp3.CDU1
+﻿namespace GrupoC.Tp3.CDU2
 {
-    partial class GenerarOrdenDePreparacionForm
+    partial class GenerarOrdenDeSeleccionForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,16 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            SuspendLayout();
-            // 
-            // GenerarOrdenDePreparacionForm
-            // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Name = "GenerarOrdenDePreparacionForm";
-            Text = "Generar Orden De Preparacion";
-            ResumeLayout(false);
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Form1";
         }
 
         #endregion
