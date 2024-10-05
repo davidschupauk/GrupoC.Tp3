1355def6c731d292620382db65f02106001ad1e1
Juanpi257 <juanpicuneo12@gmail.com>
2024-10-05 14:13:40 -0300

    Cargando datos desde el modelo

--- CDU4/GenerarDocumentoModel.cs
+++ CDU4/GenerarDocumentoModel.cs
@@ -8,5 +8,12 @@ namespace GrupoC.Tp3.CDU4
 {
     internal class GenerarDocumentoModel
     {
+    public List<Pedidos> ListaPedidos { get; private set; } =
+    [
+        new Pedidos { Orden = 160, Cliente = "Surf", Transportista = "David Schupauk", Fecha = new DateOnly(2024,12,13), Estado = "Preparado", RazonSocial = "S.A", CUIT = "20-2322", Domicilio = "Maturin 257", IdRemito = 777, Producto =  ["Teclado", "Mouse", "Monitor"], Cantidad = [3,2,1], FechaDocumento = DateTime.Now, Total = 150000  },
+        new Pedidos { Orden = 784, Cliente = "EvilCorp", Transportista = "Juan Cuneo", Fecha = new DateOnly(2024,06,29), Estado = "Preparado", RazonSocial = "S.A", CUIT = "20-2322", Domicilio = "Maturin 257", IdRemito = 777, Producto = ["Teclado", "Mouse", "Monitor"], Cantidad = [3,2,1], FechaDocumento = DateTime.Now, Total = 150000  },
+        new Pedidos { Orden = 257, Cliente = "Rappi", Transportista = "Tomas Lekerman", Fecha = new DateOnly(2024,02,01), Estado = "Preparado", RazonSocial = "S.A", CUIT = "20-2322", Domicilio = "Maturin 257", IdRemito = 777, Producto = ["Teclado", "Mouse", "Monitor"], Cantidad = [3,2,1], FechaDocumento = DateTime.Now, Total = 150000  },
+    ];
+
     }
 }
---
+++ CDU4/Pedidos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrupoC.Tp3.CDU4
+{
+    internal class Pedidos
+    {
+        public int Orden { get; set; }
+        public string Cliente { get; set; }
+        public string Transportista { get; set; }
+        public DateOnly Fecha { get; set; }
+        public string Estado { get; set; }
+        public string RazonSocial { get; set; }
+        public string CUIT { get; set; }
+        public string Domicilio { get; set; }
+        public int IdRemito { get; set; }
+        public string[] Producto { get; set; }
+        public int[] Cantidad { get; set; }
+        public DateTime FechaDocumento { get; set; }
+        public int Total { get; set; }
+    }
+}
