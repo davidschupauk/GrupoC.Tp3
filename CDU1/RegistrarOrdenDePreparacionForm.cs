9364482bf99eb752db67db689dc1362e727cb160
jose <leorodrigul17@gmail.com>
2024-10-05 08:14:53 -0300

    Casos de Uso 1 y 2

---
+++ CDU1/RegistrarOrdenDePreparacionForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GrupoC.Tp3.CDU1
+{
+    public partial class RegistrarOrdenDePreparacionForm : Form
+    {
+        public RegistrarOrdenDePreparacionForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
