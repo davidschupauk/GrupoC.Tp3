2765b822347ea9dc8cdbdac3ce0dfe7898241432
TOMAS EITAN LEKERMAN <tomasleker2002@gmail.com>
2024-10-11 16:42:58 -0300

    Se agregan ordenes al modelo

---
+++ CDU5/Orden.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrupoC.Tp3.CDU5
+{
+    internal class Orden
+    {
+        public int NroOrden { get; set; }
+        public string Cliente { get; set; }
+        public string Estado { get; set; }
+        public string Transportista { get; set; }
+
+    }
+}
---
+++ CDU5/ordenDetalle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrupoC.Tp3.CDU5
+{
+    internal class ordenDetalle
+    {
+        public int NroOrden { get; set; }
+        public string NombreProducto { get; set; }
+        public int Cantidad { get; set; }
+        public string UbicacionDeposito { get; set; }
+    }
+}
