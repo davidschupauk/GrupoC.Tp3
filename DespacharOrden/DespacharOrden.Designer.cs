13a3fabc35d6bb9a6f7b67d14ffa9701a08b1e60
Juanpi257 <juanpicuneo12@gmail.com>
2024-10-12 19:52:44 -0300

    Agregando pantallas de Empaquetar y Despachar

---
+++ DespacharOrden/DespacharOrden.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace GrupoC.Tp3.DespacharOrden
+{
+    partial class DespacharOrden
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ListViewItem listViewItem4 = new ListViewItem(new string[] { "101", "Axioma", "Juan Pablo" }, -1);
+            ListViewItem listViewItem5 = new ListViewItem(new string[] { "Juan", "Cuneo", "999999", "623" }, -1);
+            ListViewItem listViewItem6 = new ListViewItem(new string[] { "100", "Qontigo", "Tomas" }, -1);
+            OrdenesPorDespacharlistView = new ListView();
+            label1 = new Label();
+            NroOrden = new ColumnHeader();
+            Cliente = new ColumnHeader();
+            Transportista = new ColumnHeader();
+            listView1 = new ListView();
+            label2 = new Label();
+            SeleccionarOrdenbutton = new Button();
+            ConfirmarTransportistabutton = new Button();
+            Nombre = new ColumnHeader();
+            Apellido = new ColumnHeader();
+            DNI = new ColumnHeader();
+            CodigoTransportista = new ColumnHeader();
+            OrdenesDespachoAprobadolistView = new ListView();
+            columnHeader1 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            columnHeader3 = new ColumnHeader();
+            ConfirmarDespachobutton = new Button();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // OrdenesPorDespacharlistView
+            // 
+            OrdenesPorDespacharlistView.Columns.AddRange(new ColumnHeader[] { NroOrden, Cliente, Transportista });
+            OrdenesPorDespacharlistView.Items.AddRange(new ListViewItem[] { listViewItem4 });
+            OrdenesPorDespacharlistView.Location = new Point(12, 29);
+            OrdenesPorDespacharlistView.Name = "OrdenesPorDespacharlistView";
+            OrdenesPorDespacharlistView.Size = new Size(317, 299);
+            OrdenesPorDespacharlistView.TabIndex = 0;
+            OrdenesPorDespacharlistView.UseCompatibleStateImageBehavior = false;
+            OrdenesPorDespacharlistView.View = View.Details;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 11);
+            label1.Name = "label1";
+            label1.Size = new Size(129, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Ordenes por despachar";
+            // 
+            // NroOrden
+            // 
+            NroOrden.Text = "Nro Orden";
+            NroOrden.Width = 100;
+            // 
+            // Cliente
+            // 
+            Cliente.Text = "Cliente";
+            Cliente.Width = 100;
+            // 
+            // Transportista
+            // 
+            Transportista.Text = "Transportista";
+            Transportista.Width = 100;
+            // 
+            // listView1
+            // 
+            listView1.Columns.AddRange(new ColumnHeader[] { Nombre, Apellido, DNI, CodigoTransportista });
+            listView1.Items.AddRange(new ListViewItem[] { listViewItem5 });
+            listView1.Location = new Point(370, 29);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(589, 299);
+            listView1.TabIndex = 2;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(370, 11);
+            label2.Name = "label2";
+            label2.Size = new Size(106, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Datos transportista";
+            label2.Click += label2_Click;
+            // 
+            // SeleccionarOrdenbutton
+            // 
+            SeleccionarOrdenbutton.Location = new Point(254, 334);
+            SeleccionarOrdenbutton.Name = "SeleccionarOrdenbutton";
+            SeleccionarOrdenbutton.Size = new Size(75, 23);
+            SeleccionarOrdenbutton.TabIndex = 4;
+            SeleccionarOrdenbutton.Text = "Seleccionar";
+            SeleccionarOrdenbutton.UseVisualStyleBackColor = true;
+            // 
+            // ConfirmarTransportistabutton
+            // 
+            ConfirmarTransportistabutton.Location = new Point(812, 334);
+            ConfirmarTransportistabutton.Name = "ConfirmarTransportistabutton";
+            ConfirmarTransportistabutton.Size = new Size(147, 23);
+            ConfirmarTransportistabutton.TabIndex = 5;
+            ConfirmarTransportistabutton.Text = "Confirmar transportista";
+            ConfirmarTransportistabutton.UseVisualStyleBackColor = true;
+            // 
+            // Nombre
+            // 
+            Nombre.Text = "Nombre";
+            Nombre.Width = 100;
+            // 
+            // Apellido
+            // 
+            Apellido.Text = "Apellido";
+            Apellido.Width = 100;
+            // 
+            // DNI
+            // 
+            DNI.Text = "DNI";
+            DNI.Width = 100;
+            // 
+            // CodigoTransportista
+            // 
+            CodigoTransportista.Text = "Codigo Transportista";
+            CodigoTransportista.Width = 130;
+            // 
+            // OrdenesDespachoAprobadolistView
+            // 
+            OrdenesDespachoAprobadolistView.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3 });
+            OrdenesDespachoAprobadolistView.Items.AddRange(new ListViewItem[] { listViewItem6 });
+            OrdenesDespachoAprobadolistView.Location = new Point(12, 374);
+            OrdenesDespachoAprobadolistView.Name = "OrdenesDespachoAprobadolistView";
+            OrdenesDespachoAprobadolistView.Size = new Size(317, 140);
+            OrdenesDespachoAprobadolistView.TabIndex = 6;
+            OrdenesDespachoAprobadolistView.UseCompatibleStateImageBehavior = false;
+            OrdenesDespachoAprobadolistView.View = View.Details;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "Nro Orden";
+            columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "Cliente";
+            columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "Transportista";
+            columnHeader3.Width = 100;
+            // 
+            // ConfirmarDespachobutton
+            // 
+            ConfirmarDespachobutton.Location = new Point(197, 520);
+            ConfirmarDespachobutton.Name = "ConfirmarDespachobutton";
+            ConfirmarDespachobutton.Size = new Size(132, 23);
+            ConfirmarDespachobutton.TabIndex = 7;
+            ConfirmarDespachobutton.Text = "Confirmar despacho";
+            ConfirmarDespachobutton.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 356);
+            label3.Name = "label3";
+            label3.Size = new Size(208, 15);
+            label3.TabIndex = 8;
+            label3.Text = "Ordenes con transportista confirmado";
+            // 
+            // DespacharOrden
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(971, 560);
+            Controls.Add(label3);
+            Controls.Add(ConfirmarDespachobutton);
+            Controls.Add(OrdenesDespachoAprobadolistView);
+            Controls.Add(ConfirmarTransportistabutton);
+            Controls.Add(SeleccionarOrdenbutton);
+            Controls.Add(label2);
+            Controls.Add(listView1);
+            Controls.Add(label1);
+            Controls.Add(OrdenesPorDespacharlistView);
+            Name = "DespacharOrden";
+            Text = "DespacharOrden";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListView OrdenesPorDespacharlistView;
+        private ColumnHeader NroOrden;
+        private ColumnHeader Cliente;
+        private ColumnHeader Transportista;
+        private Label label1;
+        private ListView listView1;
+        private Label label2;
+        private Button SeleccionarOrdenbutton;
+        private Button ConfirmarTransportistabutton;
+        private ColumnHeader Nombre;
+        private ColumnHeader Apellido;
+        private ColumnHeader DNI;
+        private ColumnHeader CodigoTransportista;
+        private ListView OrdenesDespachoAprobadolistView;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private Button ConfirmarDespachobutton;
+        private Label label3;
+    }
+}
---
+++ EmpaquetarOrden/EmpaquetarOrden.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace GrupoC.Tp3.CDU6
+{
+    partial class EmpaquetarOrden
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ListViewItem listViewItem1 = new ListViewItem(new string[] { "101", "Axioma" }, -1);
+            ListViewItem listViewItem2 = new ListViewItem(new string[] { "Teclado", "5" }, -1);
+            ListViewItem listViewItem3 = new ListViewItem(new string[] { "Monitor", "10" }, -1);
+            OrdenesEmpaquetarlistView = new ListView();
+            NroOrden = new ColumnHeader();
+            Cliente = new ColumnHeader();
+            label1 = new Label();
+            OrdenesDetallesEmpaquetarlistView = new ListView();
+            Producto = new ColumnHeader();
+            Cantidad = new ColumnHeader();
+            label2 = new Label();
+            MostrarDetallebutton = new Button();
+            OrdenEmpaquetadabutton = new Button();
+            SuspendLayout();
+            // 
+            // OrdenesEmpaquetarlistView
+            // 
+            OrdenesEmpaquetarlistView.Columns.AddRange(new ColumnHeader[] { NroOrden, Cliente });
+            OrdenesEmpaquetarlistView.Items.AddRange(new ListViewItem[] { listViewItem1 });
+            OrdenesEmpaquetarlistView.Location = new Point(12, 38);
+            OrdenesEmpaquetarlistView.Name = "OrdenesEmpaquetarlistView";
+            OrdenesEmpaquetarlistView.Size = new Size(280, 349);
+            OrdenesEmpaquetarlistView.TabIndex = 0;
+            OrdenesEmpaquetarlistView.UseCompatibleStateImageBehavior = false;
+            OrdenesEmpaquetarlistView.View = View.Details;
+            // 
+            // NroOrden
+            // 
+            NroOrden.Text = "Nro Orden";
+            NroOrden.Width = 90;
+            // 
+            // Cliente
+            // 
+            Cliente.Text = "Cliente";
+            Cliente.Width = 90;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(139, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Ordenes por empaquetar";
+            // 
+            // OrdenesDetallesEmpaquetarlistView
+            // 
+            OrdenesDetallesEmpaquetarlistView.Columns.AddRange(new ColumnHeader[] { Producto, Cantidad });
+            OrdenesDetallesEmpaquetarlistView.Items.AddRange(new ListViewItem[] { listViewItem2, listViewItem3 });
+            OrdenesDetallesEmpaquetarlistView.Location = new Point(335, 38);
+            OrdenesDetallesEmpaquetarlistView.Name = "OrdenesDetallesEmpaquetarlistView";
+            OrdenesDetallesEmpaquetarlistView.Size = new Size(594, 349);
+            OrdenesDetallesEmpaquetarlistView.TabIndex = 2;
+            OrdenesDetallesEmpaquetarlistView.UseCompatibleStateImageBehavior = false;
+            OrdenesDetallesEmpaquetarlistView.View = View.Details;
+            // 
+            // Producto
+            // 
+            Producto.Text = "Producto";
+            Producto.Width = 70;
+            // 
+            // Cantidad
+            // 
+            Cantidad.Text = "Cantidad";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(335, 20);
+            label2.Name = "label2";
+            label2.Size = new Size(48, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Detalles";
+            // 
+            // MostrarDetallebutton
+            // 
+            MostrarDetallebutton.Location = new Point(27, 393);
+            MostrarDetallebutton.Name = "MostrarDetallebutton";
+            MostrarDetallebutton.Size = new Size(124, 23);
+            MostrarDetallebutton.TabIndex = 4;
+            MostrarDetallebutton.Text = "Mostrar detalle";
+            MostrarDetallebutton.UseVisualStyleBackColor = true;
+            // 
+            // OrdenEmpaquetadabutton
+            // 
+            OrdenEmpaquetadabutton.Location = new Point(157, 393);
+            OrdenEmpaquetadabutton.Name = "OrdenEmpaquetadabutton";
+            OrdenEmpaquetadabutton.Size = new Size(135, 23);
+            OrdenEmpaquetadabutton.TabIndex = 5;
+            OrdenEmpaquetadabutton.Text = "Orden empaquetada";
+            OrdenEmpaquetadabutton.UseVisualStyleBackColor = true;
+            // 
+            // EmpaquetarOrden
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(941, 496);
+            Controls.Add(OrdenEmpaquetadabutton);
+            Controls.Add(MostrarDetallebutton);
+            Controls.Add(label2);
+            Controls.Add(OrdenesDetallesEmpaquetarlistView);
+            Controls.Add(label1);
+            Controls.Add(OrdenesEmpaquetarlistView);
+            Name = "EmpaquetarOrden";
+            Text = "EmpaquetarOrden";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListView OrdenesEmpaquetarlistView;
+        private Label label1;
+        private ColumnHeader NroOrden;
+        private ListView OrdenesDetallesEmpaquetarlistView;
+        private ColumnHeader Producto;
+        private ColumnHeader Cantidad;
+        private Label label2;
+        private Button MostrarDetallebutton;
+        private ColumnHeader Cliente;
+        private Button OrdenEmpaquetadabutton;
+    }
+}
